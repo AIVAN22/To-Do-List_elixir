5d67019789a0ed4bbfa1569acbbcf64033068a05
Alexander <orange550@protonmail.com>
2023-04-17 07:23:47 +0300

    Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+**ToDoList**
+
+This is a simple ToDoList app built with Elixir. You can use it to manage your tasks and keep track of what needs to be done.
+
+**Installation**
+
+To run this app, you need to have Elixir installed on your machine. Once you have Elixir installed, follow these steps:
+
+1. Clone this repository to your local machine
+2. Navigate to the project directory in your terminal
+3. Run the following command to install dependencies:
+
+  mix deps.get
+
+Start the app with the following command:
+
+  mix run --no-halt
+
+**Usage**
+
+Once the app is running, you can interact with it through the command line interface. Here are the available commands:
+
+  1.  Add task
+  2.  Edit task
+  3.  Delete task
+  4.  List tasks
+  5.  Quit
+
+To select a command, enter the corresponding number and follow the prompts. You can add as many tasks as you like, edit existing tasks, delete tasks, and list all tasks currently in the app. To quit the app, select option 5.
---
+++ task-module.ex
@@ -0,0 +1,80 @@
+defmodule ToDoTasks do
+  import List, only: [replace_at: 3, delete_at: 2]
+
+  def run do
+    tasks = []
+    loop(tasks)
+  end
+
+  def loop(tasks) do
+    IO.puts("Choose an action: ")
+    IO.puts("1. Add task")
+    IO.puts("2. Edit task")
+    IO.puts("3. Delete task")
+    IO.puts("4. List task")
+    IO.puts("5. Quit")
+
+    data = IO.gets("Enter choice: ") |> String.trim() |> String.to_integer()
+
+    tasks =
+      case data do
+        1 -> add_task(tasks)
+        2 -> edit_task(tasks)
+        3 -> delete_task(tasks)
+        4 -> list_task(tasks)
+        5 -> tasks
+        _ ->
+          IO.puts("Invalid input\n")
+          tasks
+      end
+    if data != 5 do
+      loop(tasks)
+    end
+    IO.puts("Good bye")
+  end
+
+  def add_task(tasks) do
+    task = IO.gets("Enter a task: ") |> String.trim()
+    tasks ++ [task]
+  end
+
+  def edit_task(tasks) do
+    index = IO.gets("Enter the number of the task you want to edit: ") |> String.trim() |> String.to_integer()
+
+    tasks =
+      case Enum.at(tasks, index - 1) do
+        nil ->
+          IO.puts("Task not found\n")
+          tasks
+        _task ->
+          new_task = IO.gets("Enter a new description for the task: ") |> String.trim()
+          replace_at(tasks, index - 1, new_task)
+      end
+    tasks
+  end
+
+  def delete_task(tasks) do
+    index = IO.gets("Enter the number of the task you want to delete: ") |> String.trim() |> String.to_integer()
+
+    tasks =
+      case Enum.at(tasks, index - 1) do
+        nil ->
+          IO.puts("Task not found\n")
+          tasks
+        _task ->
+          delete_at(tasks, index - 1)
+      end
+
+    tasks
+  end
+
+  def list_task(tasks) do
+    Enum.with_index(tasks, fn task, index ->
+      IO.puts("#{index + 1}. #{task}")
+    end)
+
+    tasks
+  end
+end
+
+ToDoTasks.run()
